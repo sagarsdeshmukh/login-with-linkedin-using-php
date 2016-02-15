4188faf7984f666ec526af10e44e9fd8adafe061
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2016-02-15 17:18:15 +0530

    Added functionality for sign up with linked using OAuth client

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$baseURL = 'http://10.10.11.190/~sagard/login-with-linkedin-using-php/';
+$callbackURL = 'http://10.10.11.190/~sagard/login-with-linkedin-using-php/process.php';
+$linkedinApiKey = '756jf5r3runxin';
+$linkedinApiSecret = 'nf1WOgvGPOTsaIM5';
+$linkedinScope = 'r_basicprofile r_emailaddress';
+?>
